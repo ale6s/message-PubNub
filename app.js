6e66dc2149db46e983f6a103638f03dd8be6a3eb
kiritox123 <kiritox123@gmail.com>
2020-03-13 20:45:53 -0500

    comments

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
+//generates a user in this case am not using it
 const uuid = PubNub.generateUUID();
+//config to connect to yuor pubnub account
 const pubnub = new PubNub({
-    publishKey: "pub-c-2e54b131-ba35-48e6-8d70-709d53dfbc7e",
-    subscribeKey: "sub-c-99d96274-62df-11ea-9a99-f2f107c29c38",
+    publishKey: "pub-c-key goes here",
+    subscribeKey: "sub-c-key goes here",
     ssl: true,
     uuid: uuid
 });
-
+//send the text to the pubnub
 function sendmessage(txt) {
     pubnub.publish({
+        //your channel goes here
         channel: "map-AroundCabo",
         message: {
             text: txt
         }
     })
 }
+//displaying the text here
 pubnub.addListener({
     message: function (m) {
         document.getElementById("newmsg").innerHTML += "<br>" + m.message.text+"<hr>";
     }
 });
+//channnels goes here
 pubnub.subscribe({
     channels: ["map-AroundCabo"]
 });
-
+//sending the input
 function sendinput() {
     sendmessage(document.getElementById("message").value);
     document.getElementById("message").value = "";
